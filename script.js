0122ee7a0d4a70bf77da355dc9798accf75d874f
kdoh52 <kdoh52@gmail.com>
2020-08-10 18:07:38 -0700

    functioning forecast

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var current = document.getElementById("currentWeather")
+var current = document.getElementById("currentWeather");
+var forecast = document.getElementById("forecastWeather");
 
 $("#searchCity").on("click", function(event) {
     event.preventDefault();
@@ -6,7 +7,9 @@ $("#searchCity").on("click", function(event) {
     var cityName = $("#cityName").val().trim();
 
     $("#currentWeather").empty();
+    $("#forecastWeather").empty();
     currentWeather(cityName);
+    forecastWeather(cityName);
 });
 
 function currentWeather(cityName) {
@@ -25,15 +28,16 @@ function currentWeather(cityName) {
         // console.log("Wind Speed: " + response.wind.speed + " MPH");
 
         $("<h1>" + response.name + " (" + moment().format('l') + ")" + "</h1>").appendTo(current);
-        $("#currentWeather").append('<img id="weatherIcon" src="' + srcLink + '" />')
-        // $("<p>" + "Date: " + moment().format('L') + "</p>").appendTo(current);
+        $("#currentWeather").append('<img id="weatherIcon" src="' + srcLink + '" />');
         $("<p>" + "Temperature: " + (response.main.temp * 9 / 5 - 459.67).toFixed(1) + " °F" + "</p>").appendTo(current);
         $("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>").appendTo(current);
         $("<p>" + "Wind Speed: " + response.wind.speed + " MPH" + "</p>").appendTo(current);
 
         var lat = response.coord.lat;
         var lon = response.coord.lon;
+        var city = response.name;
         getUV(lat, lon);
+        // addtoHistory(city);
     });
     
     function getUV(lat, lon) {
@@ -42,9 +46,78 @@ function currentWeather(cityName) {
             url: uvURL,
             method: "GET"
         }).then(function(response) {
-            $("<p>" + "UV Index: " + response.value + "</p>").appendTo(current);
+            $("<p id='uv'>" + "UV Index: " + response.value + "</p>").appendTo(current);
+            // var uv = $("#uv")
+            if (response.value > 8) {
+                $("#uv").css('color', 'rgb(206, 45, 45)');
+            } 
+            else if (response.value > 4) {
+                $("#uv").css('color', 'rgb(245, 147, 0)');
+            }
+            else {
+                $("#uv").css('color', 'rgb(0, 194, 19)');
+            }
         })
     }
+
+    // function addtoHistory(city) {
+    //     history.push(city);
+    //     console.log(history);
+    // }
+}
+
+function forecastWeather(cityName) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=cc1d7110e10d9b9390a02a70dc1628f5"
+    // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        var srcLink1 = "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png"
+        var srcLink2 = "http://openweathermap.org/img/wn/" + response.list[1].weather[0].icon + "@2x.png"
+        var srcLink3 = "http://openweathermap.org/img/wn/" + response.list[2].weather[0].icon + "@2x.png"
+        var srcLink4 = "http://openweathermap.org/img/wn/" + response.list[3].weather[0].icon + "@2x.png"
+        var srcLink5 = "http://openweathermap.org/img/wn/" + response.list[4].weather[0].icon + "@2x.png"
+
+        console.log(response);
+        $("<h1>" + "5-Day Forecast" + "</h1>").appendTo(forecast);
+
+        $("<div class='forecastDivs' id='plusOne'>"+"</div>").appendTo(forecast);
+        var plusOne = $("#plusOne")
+        $("<h3>" + moment().add(1, 'days').format('l') + "</h3>").appendTo(plusOne);
+        $("#plusOne").append('<img id="iconOne" src="' + srcLink1 + '" />')
+        $("<p>" + "Temp: " + (response.list[0].main.temp * 9 / 5 - 459.67).toFixed(1) + " °F" + "</p>").appendTo(plusOne);
+        $("<p>" + "Humidity: " + response.list[0].main.humidity + "%" + "</p>").appendTo(plusOne);
+
+        $("<div class='forecastDivs' id='plusTwo'>"+"</div>").appendTo(forecast);
+        var plusTwo = $("#plusTwo")
+        $("<h3>" + moment().add(2, 'days').format('l') + "</h3>").appendTo(plusTwo);
+        $("#plusTwo").append('<img id="iconTwo" src="' + srcLink2 + '" />')
+        $("<p>" + "Temp: " + (response.list[1].main.temp * 9 / 5 - 459.67).toFixed(1) + " °F" + "</p>").appendTo(plusTwo);
+        $("<p>" + "Humidity: " + response.list[1].main.humidity + "%" + "</p>").appendTo(plusTwo);
+
+        $("<div class='forecastDivs' id='plusThree'>"+"</div>").appendTo(forecast);
+        var plusThree = $("#plusThree")
+        $("<h3>" + moment().add(3, 'days').format('l') + "</h3>").appendTo(plusThree);
+        $("#plusThree").append('<img id="iconThree" src="' + srcLink3 + '" />')
+        $("<p>" + "Temp: " + (response.list[2].main.temp * 9 / 5 - 459.67).toFixed(1) + " °F" + "</p>").appendTo(plusThree);
+        $("<p>" + "Humidity: " + response.list[2].main.humidity + "%" + "</p>").appendTo(plusThree);
+
+        $("<div class='forecastDivs' id='plusFour'>"+"</div>").appendTo(forecast);
+        var plusFour = $("#plusFour")
+        $("<h3>" + moment().add(4, 'days').format('l') + "</h3>").appendTo(plusFour);
+        $("#plusFour").append('<img id="iconFour" src="' + srcLink4 + '" />')
+        $("<p>" + "Temp: " + (response.list[3].main.temp * 9 / 5 - 459.67).toFixed(1) + " °F" + "</p>").appendTo(plusFour);
+        $("<p>" + "Humidity: " + response.list[3].main.humidity + "%" + "</p>").appendTo(plusFour);
+
+        $("<div class='forecastDivs' id='plusFive'>"+"</div>").appendTo(forecast);
+        var plusFive = $("#plusFive")
+        $("<h3>" + moment().add(5, 'days').format('l') + "</h3>").appendTo(plusFive);
+        $("#plusFive").append('<img id="iconFive" src="' + srcLink5 + '" />')
+        $("<p>" + "Temp: " + (response.list[4].main.temp * 9 / 5 - 459.67).toFixed(1) + " °F" + "</p>").appendTo(plusFive);
+        $("<p>" + "Humidity: " + response.list[4].main.humidity + "%" + "</p>").appendTo(plusFive);
+    });
 }
 
 // function kelvinToFahrenheit(kelvin) {
